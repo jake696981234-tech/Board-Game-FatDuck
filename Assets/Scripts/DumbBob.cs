da1ac589c0368edf1a011bfe8c0ce489e0d3a5a1
jake696981234-tech <jake696981234@gmail.com>
2026-01-06 06:14:26 +0800

    Starting work DumbBob

--- Assets/Scripts/DumbBob.cs
+++ Assets/Scripts/DumbBob.cs
@@ -1,6 +1,35 @@
 using UnityEngine;
+using System;
+using Game.Core;
+using Action = Game.Core.Action;
+using static Game.Core.ActionKind; // import enum values
 
 public class DumbBob
 {
-    
+    public int PickAction(in OfferQuery q,
+                          ReadOnlySpan<Action> acts,
+                          ReadOnlySpan<float> costs,
+                          ReadOnlySpan<byte> mask,
+                          int gameIndex,
+                          byte playerId)
+    {
+        var gameState = GameRegistry.game[gameIndex].gameState;
+        //Gate to sperfic factions
+        
+        for (int i = 0; i < acts.Length; i++)
+        {
+            countOfSortedActions[acts[i].kind]++;
+            sortedActions[acts[i].kind, countOfSortedActions[acts[i].kind]] = i;
+        }
+        if (countOfSortedActions[CaptureVP] > 0) return sortedActions[CaptureVP, 0];
+        if (countOfSortedActions[CoreDamage] > 0) return sortedActions[CoreDamage, 0];
+        float chance = UnityEngine.Random.value; 
+
+        if ()
+
+        return -1;
+    }
+
+    private int[,] sortedActions = new int[16 , 217]; //keyed by action kind
+    private int[] countOfSortedActions = new int [16];
 }
